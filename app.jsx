1276e6acdfee326bdd11924417ba3731e8a612af
Dieter Luypaert <dieterluypaert@gmail.com>
2020-09-30 13:05:56 +0200

    feat: add compound variables
    Everything works as expected.

--- app.jsx
+++ app.jsx
@@ -4,12 +4,27 @@ import  { styled } from './stitches.config';
 
 const Paragraph = styled('p', {
   color: 'red',
+  variants: {
+    var1: { true: { color: 'green' }, false: {} },
+    var2: { true: { color: 'blue' }, false: {} },
+  }
 });
+
+Paragraph.compoundVariant({ var1: 'true', var2: 'false' }, { fontWeight: 'bold' })
+
+Paragraph.compoundVariant({ var1: 'false', var2: 'true' }, { textDecoration: 'underline' })
+
+Paragraph.defaultProps = {
+  var1: 'false',
+  var2: 'false',
+};
 
 export function App() {
   return (
     <div>
       <Paragraph>Test</Paragraph>
+      <Paragraph var1="true">Test</Paragraph>
+      <Paragraph var2="true">Test</Paragraph>
     </div>
   )
 }
